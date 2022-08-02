acae43fb4c755772c7dfb410bacfc47f47c7dde8
TableWaterFlower <charles@redwoodplains.com>
2022-08-02 06:46:57 +0000

    Completed secret encoder

--- raindrops.rb
+++ raindrops.rb
@@ -37,3 +37,19 @@
 
 
 p "Enter an integer"
+
+# If the number has 3 as a factor, output 'Pling'.
+# If the number has 5 as a factor, output 'Plang'.
+# If the number has 7 as a factor, output 'Plong'.
+# If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+
+userInput = gets.chomp.to_i
+
+if userInput % 3 == 0 
+    p 'pling'
+elsif userInput % 5 == 0
+    p 'plang'
+elsif userInput % 7 == 0 
+    p 'plong'
+else return userInput
+end
